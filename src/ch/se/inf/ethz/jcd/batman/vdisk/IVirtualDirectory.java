a35a103a69a3ed1ca8a76037f0cf9d43cfb3ddc6
Gregor Wegberg <github@gregorwegberg.com>
2013-04-03 11:04:46 +0200

    Added JavaDoc comments to IVirtualDiskEntry.

--- src/ch/se/inf/ethz/jcd/batman/vdisk/IVirtualDirectory.java
+++ src/ch/se/inf/ethz/jcd/batman/vdisk/IVirtualDirectory.java
@@ -1,13 +1,40 @@
 package ch.se.inf.ethz.jcd.batman.vdisk;
 
-
-
+/**
+ * Represents a directory on the virtual disk.
+ * 
+ * A directory contains members ({@link IVirtualDiskEntry}) which belong to the
+ * directory. This includes files and subdirectories inside the directory. As
+ * defined by the {@link IVirtualDiskEntry} the member list is a doubly linked
+ * list.
+ * 
+ */
 public interface IVirtualDirectory extends IVirtualDiskEntry {
-	
-	void removeMember (IVirtualDiskEntry member) throws VirtualDiskException;
-	
-	void addMember (IVirtualDiskEntry member);
-	
-	IVirtualDiskEntry getFirstMember ();
-	
+
+	/**
+	 * Removes the given member from the list.
+	 * 
+	 * @param member
+	 *            member to remove from the list
+	 * @throws VirtualDiskException
+	 *             TODO
+	 */
+	void removeMember(IVirtualDiskEntry member) throws VirtualDiskException;
+
+	/**
+	 * Adds the given member to the list.
+	 * 
+	 * @param member
+	 *            member to add to the list
+	 */
+	void addMember(IVirtualDiskEntry member);
+
+	/**
+	 * Returns the first member of the list.
+	 * 
+	 * @see IVirtualDiskEntry#getNextEntry()
+	 * @return the first member of the list.
+	 */
+	IVirtualDiskEntry getFirstMember();
+
 }
--- src/ch/se/inf/ethz/jcd/batman/vdisk/IVirtualDiskEntry.java
+++ src/ch/se/inf/ethz/jcd/batman/vdisk/IVirtualDiskEntry.java
@@ -1,32 +1,131 @@
 package ch.se.inf.ethz.jcd.batman.vdisk;
 
-
+/**
+ * Represents an entry inside the virtual disk. This is the basic interface that
+ * is extended by directories ({@link IVirtualDirectory}) and files (
+ * {@link IVirtualFile}.
+ * 
+ */
 public interface IVirtualDiskEntry {
 
-	void setName (String name);
-	
-	String getName ();
+	/**
+	 * Sets the name of the entry.
+	 * 
+	 * The length of the name is only limited by the size of the disk itself.
+	 * The given name will be encoded into UTF-8 to save it on the disk.
+	 * 
+	 * @param name
+	 *            the name of the virtual disk entry
+	 */
+	void setName(String name);
 
-	IVirtualDirectory getParent ();
-	
-	void setParent (IVirtualDirectory directory);
-	
-	IVirtualDiskEntry getPreviousEntry ();
-	
-	void setPreviousEntry (IVirtualDiskEntry entry);
-	
-	IVirtualDiskEntry getNextEntry ();
-	
-	void setNextEntry (IVirtualDiskEntry entry);
-	
-	long getTimestamp ();
-	
-	void setTimestamp (long timestamp);
-	
-	void delete ();
-	
-	boolean exists ();
-	
+	/**
+	 * Returns the name of the virtual disk entry.
+	 * 
+	 * @return the name of the virtual disk entry
+	 */
+	String getName();
+
+	/**
+	 * Returns the parent directory of the virtual disk entry.
+	 * 
+	 * Only the root directory will return null.
+	 * 
+	 * @return the parent directory or null if it is the root directory itself
+	 */
+	IVirtualDirectory getParent();
+
+	/**
+	 * Sets the parent of the virtual disk entry to the given directory.
+	 * 
+	 * @param directory
+	 *            the parent of the virtual disk entry
+	 */
+	void setParent(IVirtualDirectory directory);
+
+	/**
+	 * Returns the previous virtual disk entry.
+	 * 
+	 * Every virtual disk entry (except the root directory) is part of a doubly
+	 * linked list containing virtual disk entries ({@link IVirtualDiskEntry})
+	 * that are on the same hierarchical level, therefore they have the same
+	 * parent ({@link #getParent()}). This method will return the previous entry
+	 * in this doubly linked list. It returns null if there is no previous
+	 * member.
+	 * 
+	 * @see IVirtualDirectory#getFirstMember()
+	 * @return previous member or null if there is none
+	 */
+	IVirtualDiskEntry getPreviousEntry();
+
+	/**
+	 * Sets the previous virtual disk entry.
+	 * 
+	 * @see #getPreviousEntry()
+	 * @param entry
+	 *            the new previous entry
+	 */
+	void setPreviousEntry(IVirtualDiskEntry entry);
+
+	/**
+	 * Returns the next virtual disk entry.
+	 * 
+	 * Every virtual disk entry (except the root directory) is part of a doubly
+	 * linked list containing virtual disk entries ({@link IVirtualDiskEntry})
+	 * that are on the same hierarchical level, therefore they have the same
+	 * parent ({@link #getParent()}). This method will return the next entry in
+	 * this doubly linked list. It returns null if there is no next member.
+	 * 
+	 * @return next member or null if there is none
+	 */
+	IVirtualDiskEntry getNextEntry();
+
+	/**
+	 * Sets the next virtual disk entry.
+	 * 
+	 * @see #getNextEntry()
+	 * @param entry
+	 *            the new next entry
+	 */
+	void setNextEntry(IVirtualDiskEntry entry);
+
+	/**
+	 * Returns the timestamp of the virtual disk entry.
+	 * 
+	 * @return the timestamp of the virtual disk entry
+	 */
+	long getTimestamp();
+
+	/**
+	 * Sets the timestamp to the given timestamp.
+	 * 
+	 * @param timestamp new timestamp for the virtual disk entry
+	 */
+	void setTimestamp(long timestamp);
+
+	/**
+	 * Deletes the virtual disk entry.
+	 * 
+	 * For details how this is exactly executed read the documentation of the
+	 * specific implementations.
+	 */
+	void delete();
+
+	/**
+	 * Indicates if the virtual disk entry is still on the disk or was deleted.
+	 * 
+	 * @return true if it is still on the disk, otherwise false
+	 */
+	boolean exists();
+
+	/**
+	 * Returns the position of the entry inside the virtual disk.
+	 * 
+	 * The returned position is relative to the virtual disk beginning.
+	 * Position 0 would be the first byte of the virtual disk.
+	 * 
+	 * @return the position of the entry inside the virtual disk
+	 */
 	long getPosition();
-	
+
 }
