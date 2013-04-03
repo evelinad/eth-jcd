c9eb504b229911fc937fdeeb1d47d504cebf9522
Gregor Wegberg <github@gregorwegberg.com>
2013-04-03 16:50:22 +0200

    added JavaDoc documentation to IFreeBlock

--- src/ch/se/inf/ethz/jcd/batman/vdisk/IFreeBlock.java
+++ src/ch/se/inf/ethz/jcd/batman/vdisk/IFreeBlock.java
@@ -2,14 +2,59 @@ package ch.se.inf.ethz.jcd.batman.vdisk;
 
 import java.io.IOException;
 
+/**
+ * Represents a block that is not used to save useful data.
+ * 
+ * This interface allows to create an implicit doubly linked list of free blocks
+ * in the virtual disk. Large parts, which are normally used to store data (see
+ * {@link IDataBlock}), are not used and may contain garbage.
+ * 
+ * @see IVirtualBlock
+ * @see IDataBlock
+ */
 public interface IFreeBlock extends IVirtualBlock {
 
-	long getNextBlock();
+    /**
+     * Returns the position (address) of the next block inside the implicit
+     * doubly linked list of free blocks.
+     * 
+     * The address is relative to the beginning of the virtual disk. Therefore
+     * position 0 is the first byte of the virtual disk.
+     * 
+     * @return position of the next free block, zero if there is none
+     */
+    long getNextBlock();
 
-	void setNextBlock(long nextBlock) throws IOException;
+    /**
+     * Sets the position of the next free block.
+     * 
+     * @see #getNextBlock()
+     * @param nextBlock
+     *            the position of the next free block.
+     * @throws IOException
+     *             TODO
+     */
+    void setNextBlock(long nextBlock) throws IOException;
 
-	long getPreviousBlock();
-	
-	void setPreviousBlock(long previousBlock) throws IOException;
-	
+    /**
+     * Returns the position (address) of the previous block inside the implicit
+     * doubly linked list of free blocks.
+     * 
+     * The address is relative to the beginning of the virtual disk. Therefore
+     * position 0 is the first byte of the virtual disk.
+     * 
+     * @return position of the previous free block, zero if there is none
+     */
+    long getPreviousBlock();
+
+    /**
+     * Sets the position of the previous free block.
+     * 
+     * @param previousBlock
+     *            the position of the previous free block
+     * @throws IOException
+     *             TODO
+     */
+    void setPreviousBlock(long previousBlock) throws IOException;
+
 }
