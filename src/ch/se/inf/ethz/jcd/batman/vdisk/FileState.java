bcb348c63b4f51dbed7cfb49c5aed8ba771bb347
Gregor Wegberg <github@gregorwegberg.com>
2013-04-03 15:21:23 +0200

    added JavaDoc documentation to FileState

--- src/ch/se/inf/ethz/jcd/batman/vdisk/FileState.java
+++ src/ch/se/inf/ethz/jcd/batman/vdisk/FileState.java
@@ -1,5 +1,21 @@
 package ch.se.inf.ethz.jcd.batman.vdisk;
 
+/**
+ * Represents the states in which a virtual disk entry
+ * ({@link IVirtualDiskEntry}, {@link VirtualDiskEntry}) can be.
+ * 
+ * @see VirtualDiskEntry
+ */
 public enum FileState {
-	CREATED, DELETED;
+	/**
+	 * Represents a state in which the virtual disk entry is still valid and
+	 * available on disk.
+	 */
+	CREATED,
+
+	/**
+	 * Represents a state in which the virtual disk entry is not valid anymore
+	 * and not on disk anymore.
+	 */
+	DELETED;
 }
