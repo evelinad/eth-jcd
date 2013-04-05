0cdfc342758393c083cbeb7d6b45c750e781735f
Gregor Wegberg <github@gregorwegberg.com>
2013-04-05 14:01:56 +0200

    fixed to early call to setHandled()

--- src/ch/se/inf/ethz/jcd/batman/cli/command/StopCommand.java
+++ src/ch/se/inf/ethz/jcd/batman/cli/command/StopCommand.java
@@ -11,41 +11,42 @@ import ch.se.inf.ethz.jcd.batman.vdisk.IVirtualDisk;
  * Implements a quit command that can be used to end the CLI.
  */
 public class StopCommand implements PrioritizedObserver<String> {
-	/**
-	 * Accepted commands. Case will be ignored for the check.
-	 */
-	private static final String[] COMMAND_STRINGS = {"quit", "q", "exit", "stop"};
+    /**
+     * Accepted commands. Case will be ignored for the check.
+     */
+    private static final String[] COMMAND_STRINGS = { "quit", "q", "exit",
+            "stop" };
 
-	@Override
-	public void update(PrioritizedObservable<String> obs, String line) {
-		assert obs instanceof CommandLineInterface;
-		CommandLineInterface cli = (CommandLineInterface)obs;
-		
-		for(String commandStr : COMMAND_STRINGS) {
-            cli.setHandled();
-            
-			if(line.equalsIgnoreCase(commandStr)) {
-				cli.writeln("exiting...");
-				
-				IVirtualDisk disk = cli.getCurrentLocation().getDisk();
-				if(disk != null) {
-				    try {
+    @Override
+    public void update(PrioritizedObservable<String> obs, String line) {
+        assert obs instanceof CommandLineInterface;
+        CommandLineInterface cli = (CommandLineInterface) obs;
+
+        for (String commandStr : COMMAND_STRINGS) {
+
+            if (line.equalsIgnoreCase(commandStr)) {
+                cli.setHandled();
+                cli.writeln("exiting...");
+
+                IVirtualDisk disk = cli.getCurrentLocation().getDisk();
+                if (disk != null) {
+                    try {
                         disk.close();
                     } catch (IOException ex) {
                         cli.writeln(String.format(
                                 "following exception occured: %s",
                                 ex.getMessage()));
                     }
-				}
-				
-				cli.stop();
-			}
-		}
-	}
+                }
 
-	@Override
-	public int getPriority() {
-		return Integer.MAX_VALUE - 1;
-	}
+                cli.stop();
+            }
+        }
+    }
+
+    @Override
+    public int getPriority() {
+        return Integer.MAX_VALUE - 1;
+    }
 
 }
