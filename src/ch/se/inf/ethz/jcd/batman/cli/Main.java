ace4fda5a7f475ec3f53b0c0ab1604cf732eddec
Gregor Wegberg <github@gregorwegberg.com>
2013-04-05 19:18:47 +0200

    implemented import command for CLI

--- src/ch/se/inf/ethz/jcd/batman/cli/Main.java
+++ src/ch/se/inf/ethz/jcd/batman/cli/Main.java
@@ -6,6 +6,7 @@ import ch.se.inf.ethz.jcd.batman.cli.command.ChangeDirectoryCommand;
 import ch.se.inf.ethz.jcd.batman.cli.command.CommandNotFound;
 import ch.se.inf.ethz.jcd.batman.cli.command.CreateCommand;
 import ch.se.inf.ethz.jcd.batman.cli.command.CreateDirectoryCommand;
+import ch.se.inf.ethz.jcd.batman.cli.command.ImportCommand;
 import ch.se.inf.ethz.jcd.batman.cli.command.ListMembersCommand;
 import ch.se.inf.ethz.jcd.batman.cli.command.LoadCommand;
 import ch.se.inf.ethz.jcd.batman.cli.command.MoveCommand;
@@ -33,6 +34,7 @@ public class Main {
 		cli.addObserver(new CreateDirectoryCommand());
 		cli.addObserver(new MoveCommand());
 		cli.addObserver(new SizeCommand());
+		cli.addObserver(new ImportCommand());
 		
 		// start command line interface
 		cli.start();
---
+++ src/ch/se/inf/ethz/jcd/batman/cli/command/ImportCommand.java
@@ -0,0 +1,116 @@
+package ch.se.inf.ethz.jcd.batman.cli.command;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.InvalidPathException;
+
+import ch.se.inf.ethz.jcd.batman.cli.CommandLineInterface;
+import ch.se.inf.ethz.jcd.batman.io.VDiskFile;
+import ch.se.inf.ethz.jcd.batman.io.VDiskFileOutputStream;
+import ch.se.inf.ethz.jcd.batman.util.PrioritizedObservable;
+import ch.se.inf.ethz.jcd.batman.util.PrioritizedObserver;
+import ch.se.inf.ethz.jcd.batman.vdisk.IVirtualDisk;
+
+public class ImportCommand implements PrioritizedObserver<String> {
+    private static final String[] COMMAND_STRINGS = { "import" };
+
+    @Override
+    public void update(PrioritizedObservable<String> observable, String data) {
+        assert observable instanceof CommandLineInterface;
+        CommandLineInterface cli = (CommandLineInterface) observable;
+
+        String[] lineParts = data.split(" ");
+        for (String command : COMMAND_STRINGS) {
+            if (lineParts[0].equalsIgnoreCase(command)) {
+                cli.setHandled();
+
+                // parse parameters
+                try {
+                    if (lineParts.length == 3) {
+                        // extract host path
+                        File hostFile = null;
+                        try {
+                            hostFile = new File(lineParts[1]);
+
+                            if (!hostFile.exists()) {
+                                cli.writeln(String.format(
+                                        "host file '%s' does not exist",
+                                        hostFile.getAbsolutePath()));
+                                return;
+                            }
+
+                            if (!hostFile.isFile()) {
+                                cli.writeln(String.format(
+                                        "host file '%s' is not a file",
+                                        hostFile.getAbsolutePath()));
+                                return;
+                            }
+                        } catch (InvalidPathException ex) {
+                            cli.writeln(String.format(
+                                    "provided path is not valid: %s",
+                                    ex.getMessage()));
+                            return;
+                        }
+
+                        // extract virtual path
+                        VDiskFile currentLocation = cli.getCurrentLocation();
+                        String virtualPathParam = lineParts[2];
+
+                        VDiskFile virtualFile = null;
+
+                        if (virtualPathParam.startsWith(String
+                                .valueOf(IVirtualDisk.PATH_SEPARATOR))) {
+                            virtualFile = new VDiskFile(virtualPathParam,
+                                    currentLocation.getDisk());
+                        } else {
+                            virtualFile = new VDiskFile(currentLocation,
+                                    virtualPathParam);
+                        }
+
+                        if (!virtualFile.exists()) {
+                            virtualFile.createNewFile();
+                        } else {
+                            cli.writeln(String.format(
+                                    "virtual file '%s' already exists",
+                                    virtualFile.getPath()));
+                        }
+
+                        // create streams
+                        FileInputStream reader = new FileInputStream(hostFile);
+
+                        VDiskFileOutputStream writer = new VDiskFileOutputStream(
+                                virtualFile.getPath(), virtualFile.getDisk());
+
+                        // import
+                        int read = 0;
+                        byte[] buffer = new byte[1024];
+                        do {
+                            read = reader.read(buffer);
+                            writer.write(buffer);
+                        } while (read != 0);
+
+                        writer.close();
+                        reader.close();
+
+                        cli.writeln(String.format("imported '%s' into '%s'",
+                                hostFile.getAbsolutePath(),
+                                virtualFile.getPath()));
+
+                    } else {
+                        cli.writeln("not the right amount of parameters provided.");
+                    }
+                } catch (IOException ex) {
+                    cli.writeln(String.format(
+                            "following exception occured: %s", ex.getMessage()));
+                }
+            }
+        }
+    }
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
+
+}
