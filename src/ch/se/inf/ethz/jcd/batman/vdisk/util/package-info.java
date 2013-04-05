8c71541b81f1731f7cf0f4358173f505823560f8
Gregor Wegberg <github@gregorwegberg.com>
2013-04-05 10:55:14 +0200

    Added package-info.java to ch.se.inf.ethz.jcd.batman.vdisk.util

---
+++ src/ch/se/inf/ethz/jcd/batman/vdisk/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains utility classes which can be used for easier handling of the
+ * virtual disk. Some of this classes are used by the virtual disk
+ * implementation itself.
+ */
+package ch.se.inf.ethz.jcd.batman.vdisk.util;
