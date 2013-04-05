960ee8cdf9d509ced8087eb0f16daa68cb9feb8f
Gregor Wegberg <github@gregorwegberg.com>
2013-04-05 18:33:38 +0200

    Implemented basic version of an InputStream and OutputStream

--- src/ch/se/inf/ethz/jcd/batman/io/VDiskFileOutputStream.java
+++ src/ch/se/inf/ethz/jcd/batman/io/VDiskFileOutputStream.java
@@ -1,96 +1,109 @@
 package ch.se.inf.ethz.jcd.batman.io;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
+
+import ch.se.inf.ethz.jcd.batman.vdisk.IVirtualDisk;
+import ch.se.inf.ethz.jcd.batman.vdisk.IVirtualFile;
 
 /**
  * Implements {@link java.io.OutputStream} for files on the Batman Virtual Disk.
  * 
  */
 public class VDiskFileOutputStream extends OutputStream {
-	// fields
+    // fields
+    private final IVirtualFile file;
 
-	// constructors
-	/**
-	 * Default constructor cannot be used as this output stream has to be always
-	 * associated with a specific file.
-	 * 
-	 * Internally used to initialize fields to default values.
-	 */
-	private VDiskFileOutputStream() {
-		// TODO Auto-generated constructor stub
-	}
+    // constructors
 
-	/**
-	 * Creates a VDiskFileOutputStream by opening the file named by the filePath
-	 * parameter.
-	 * 
-	 * If the file already exists it will overwrite the current content. If the
-	 * file does not yet exist the file will be created.
-	 * 
-	 * @param filePath
-	 *            path to the file to open
-	 */
-	public VDiskFileOutputStream(String filePath) {
-		this(filePath, false);
-	}
+    /**
+     * Creates a VDiskFileOutputStream by opening the file named by the filePath
+     * parameter.
+     * 
+     * If the file already exists it will overwrite the current content. If the
+     * file does not yet exist the file will be created.
+     * 
+     * @param filePath
+     *            path to the file to open
+     * @param disk
+     *            TODO
+     * @throws IOException
+     */
+    public VDiskFileOutputStream(String filePath, IVirtualDisk disk)
+            throws IOException {
+        this(filePath, disk, false);
+    }
 
-	/**
-	 * Creates a VDiskFileOutputStream by opening the file named by the filePath
-	 * parameter.
-	 * 
-	 * If the file already exists, the written content will be appended to the
-	 * current data. If the file dies not yet exist the file will be created.
-	 * 
-	 * @param filePath
-	 *            path to the file to open
-	 * @param append
-	 *            true if the written content should be appended to the current
-	 *            data.
-	 */
-	public VDiskFileOutputStream(String filePath, boolean append) {
-		this();
-		// TODO
-	}
+    /**
+     * Creates a VDiskFileOutputStream by opening the file named by the filePath
+     * parameter.
+     * 
+     * If the file already exists, the written content will be appended to the
+     * current data. If the file dies not yet exist the file will be created.
+     * 
+     * @param filePath
+     *            path to the file to open
+     * @param disk
+     *            TODO
+     * @param append
+     *            true if the written content should be appended to the current
+     *            data.
+     * @throws IOException
+     */
+    public VDiskFileOutputStream(String filePath, IVirtualDisk disk,
+            boolean append) throws IOException {
 
-	// public methods
-	@Override
-	public void close() throws IOException {
-		// TODO Auto-generated method stub
-		super.close();
-	}
+        VDiskFile fileHelper = new VDiskFile(filePath, disk);
+        if (!fileHelper.exists() || !fileHelper.isFile()) {
+            throw new FileNotFoundException();
+        }
 
-	@Override
-	public void flush() throws IOException {
-		// TODO Auto-generated method stub
-		super.flush();
-	}
+        this.file = (IVirtualFile) fileHelper.getDiskEntry();
+        if(append) {
+            this.file.seek(this.file.getSize());
+        } else {
+            this.file.seek(0);
+        }
+    }
 
-	@Override
-	public void write(int b) throws IOException {
-		// TODO Auto-generated method stub
+    // public methods
+    @Override
+    public void close() throws IOException {
+        /*
+         * not needed as virtual disk has no buffer or anything else that would
+         * need to be closed.
+         */
+    }
 
-	}
+    @Override
+    public void flush() throws IOException {
+        // not needed as virtual disk has no buffer
+    }
 
-	@Override
-	public void write(byte[] b) throws IOException {
-		// TODO Auto-generated method stub
-		super.write(b);
-	}
+    @Override
+    public void write(int b) throws IOException {
+        this.file.write((byte) b);
+    }
 
-	@Override
-	public void write(byte[] b, int off, int len) throws IOException {
-		// TODO Auto-generated method stub
-		super.write(b, off, len);
-	}
+    @Override
+    public void write(byte[] b) throws IOException {
+        this.file.write(b);
+    }
 
-	// protected methods
-	@Override
-	protected void finalize() throws Throwable {
-		// TODO Auto-generated method stub
-		super.finalize();
-	}
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        byte[] toBeWritten = Arrays.copyOfRange(b, off, len);
+        this.file.write(toBeWritten);
+    }
 
-	// private methods
+    // protected methods
+    @Override
+    protected void finalize() throws Throwable {
+        this.close();
+    }
+
+    // private methods
 
 }
