55b9de8c39e99f96b15f52637e53973b2fc820c4
Gregor Wegberg <github@gregorwegberg.com>
2013-04-06 11:10:37 +0200

    Added exportFile skeleton to HostBridge and first implementation.
    The current implementation allows only an existing file to non-existing
    file export.

--- src/ch/se/inf/ethz/jcd/batman/io/HostBridge.java
+++ src/ch/se/inf/ethz/jcd/batman/io/HostBridge.java
@@ -2,6 +2,8 @@ package ch.se.inf.ethz.jcd.batman.io;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -22,7 +24,7 @@ public class HostBridge {
      * The parent of virtualFile has to exists otherwise an
      * {@link UnsupportedOperationException} is thrown.
      * 
-     * @param hostFile
+     * @param absHostFile
      *            what to import
      * @param virtualFile
      *            where to import into
@@ -31,8 +33,14 @@ public class HostBridge {
      */
     public static void importFile(File hostFile, VDiskFile virtualFile)
             throws IOException {
+        File absHostFile = hostFile.getAbsoluteFile();
+        
+        if(!absHostFile.exists()) {
+            throw new FileNotFoundException();
+        }
+        
         if (virtualFile.exists()) {
-            if (hostFile.isFile() && virtualFile.isDirectory()) {
+            if (absHostFile.isFile() && virtualFile.isDirectory()) {
                 /*
                  * We have to move the host file into an existing virtual
                  * directory
@@ -50,13 +58,13 @@ public class HostBridge {
                 throw new FileAlreadyExistsException(virtualFile.getPath());
             }
         } else {
-            if (hostFile.isFile()) {
+            if (absHostFile.isFile()) {
                 /*
                  * Imports a file on the host system into a not yet existing
                  * file
                  */
-                importFileIntoFile(hostFile, virtualFile);
-            } else if (hostFile.isDirectory()) {
+                importFileIntoFile(absHostFile, virtualFile);
+            } else if (absHostFile.isDirectory()) {
                 throw new UnsupportedOperationException(); // TODO
             } else {
                 /*
@@ -65,6 +73,73 @@ public class HostBridge {
                 throw new UnsupportedOperationException();
             }
         }
+    }
+
+    /**
+     * Exports the given VDiskFile (may be a file or directory) into the given
+     * File on the host system.
+     * 
+     * @param virtualFile
+     *            what to export
+     * @param absHostFile
+     *            where to export to
+     * @throws IOException TODO
+     */
+    public static void exportFile(VDiskFile virtualFile, File hostFile) throws IOException {
+        File absHostFile = hostFile.getAbsoluteFile();
+        
+        if(!virtualFile.exists()) {
+            throw new FileNotFoundException();
+        }
+        
+        if(absHostFile.exists()) {
+            if(virtualFile.isFile() && absHostFile.isDirectory()) {
+                /*
+                 * export given virtual file into the given host directory
+                 */
+                throw new UnsupportedOperationException(); // TODO
+            } else {
+                /*
+                 * all other cases are not supported as we would overwrite
+                 * existing data
+                 */
+                throw new UnsupportedOperationException();
+            }
+        } else {
+            if(virtualFile.isFile()) {
+                /*
+                 * export given virtual file into the host file system by
+                 * creating the not yet existing host file
+                 */
+                if(!absHostFile.getParentFile().exists()) {
+                    throw new FileNotFoundException(absHostFile.getParent());
+                }
+                
+                exportFileIntoFile(virtualFile, absHostFile);
+            } else if(virtualFile.isDirectory()) {
+                /*
+                 * export a virtual directory into a not yet existing directory
+                 * on the host system
+                 */
+                if(!absHostFile.getParentFile().exists()) {
+                    throw new FileNotFoundException(absHostFile.getParent());
+                }
+                
+                throw new UnsupportedOperationException(); // TODO
+            } else {
+                throw new UnsupportedOperationException();
+            }
+        }
+    }
+
+    private static void exportFileIntoFile(VDiskFile virtualFile, File hostFile) throws IOException {
+        FileOutputStream writer = new FileOutputStream(hostFile);
+        VDiskFileInputStream reader = new VDiskFileInputStream(virtualFile);
+        
+        moveData(reader, writer);
+        
+        reader.close();
+        writer.close();
     }
 
     private static void importFileIntoFile(File hostFile, VDiskFile virtualFile)
@@ -83,20 +158,21 @@ public class HostBridge {
                 false);
 
         moveData(reader, writer);
-        
+
         reader.close();
         writer.close();
     }
 
-    private static void moveData(InputStream reader, OutputStream writer) throws IOException {
+    private static void moveData(InputStream reader, OutputStream writer)
+            throws IOException {
         byte[] buffer = new byte[BUFFER_SIZE];
-        
+
         int readAmount = 0;
         do {
             readAmount = reader.read(buffer);
             if (readAmount > 0) {
-            	writer.write(buffer, 0, readAmount);
+                writer.write(buffer, 0, readAmount);
             }
-        } while(readAmount > 0);
+        } while (readAmount > 0);
     }
 }
