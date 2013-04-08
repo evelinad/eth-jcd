f508edb15eb14d612ccaeb37d0ecc4464fa59c7d
Gregor Wegberg <github@gregorwegberg.com>
2013-04-08 08:06:38 +0200

    added JavaDoc comments to VirtualDiskUtil

--- src/ch/se/inf/ethz/jcd/batman/vdisk/util/VirtualDiskUtil.java
+++ src/ch/se/inf/ethz/jcd/batman/vdisk/util/VirtualDiskUtil.java
@@ -8,8 +8,29 @@ import java.util.List;
 import ch.se.inf.ethz.jcd.batman.vdisk.IVirtualDirectory;
 import ch.se.inf.ethz.jcd.batman.vdisk.IVirtualDiskEntry;
 
+/**
+ * Utility class to work with a VirtualDisk.
+ * 
+ * This class provides some static methods for convenience and to prevent code
+ * duplication.
+ * 
+ */
 public class VirtualDiskUtil {
 
+    /**
+     * Returns a collection of members of a directory.
+     * 
+     * A member of a directory is a {@link IVirtualDiskEntry} that belongs to
+     * the given directory. Therefore this method returns children of the given
+     * directory.
+     * 
+     * @param directory
+     *            the parent directory of the returned members
+     * @return a collection of members which are children of the given directory
+     * @throws IOException
+     *             in case of an {@link IOException} of the underlying virtual
+     *             disk
+     */
     public static Collection<IVirtualDiskEntry> getDirectoryMembers(
             IVirtualDirectory directory) throws IOException {
 
@@ -24,6 +45,22 @@ public class VirtualDiskUtil {
         return members;
     }
 
+    /**
+     * Searches the {@link IVirtualDiskEntry} whose parent is the given
+     * {@link IVirtualDirectory} directory and has the given name.
+     * 
+     * This method may return null if no entry was found.
+     * 
+     * @param directory
+     *            the directory in which to search for the given name
+     * @param name
+     *            the name to search inside the directory
+     * @return a {@link IVirtualDiskEntry} if the member was found, otherwise
+     *         null
+     * @throws IOException
+     *             in case of an {@link IOException} of the underlying virtual
+     *             disk
+     */
     public static IVirtualDiskEntry getDirectoryMember(
             IVirtualDirectory directory, String name) throws IOException {
 
@@ -42,6 +79,21 @@ public class VirtualDiskUtil {
         }
     }
 
+    /**
+     * Returns all members as a collection of names.
+     * 
+     * Works the same way as {@link #getDirectoryMembers(IVirtualDirectory)}.
+     * 
+     * May return an empty list if no members were found.
+     * 
+     * @see #getDirectoryMembers(IVirtualDirectory)
+     * @param directory
+     *            the parent directory of the returned members
+     * @return a list of member names or null if none found
+     * @throws IOException
+     *             in case of an {@link IOException} of the underlying virtual
+     *             disk
+     */
     public static Collection<String> getDirectoryMemberNames(
             IVirtualDirectory directory) throws IOException {
 
