bbdeb731988f7502800ecc3f6c23daa2252dce09
Gregor Wegberg <github@gregorwegberg.com>
2013-04-20 16:37:47 +0200

    Implemented RegEx support for search

--- core/src/ch/se/inf/ethz/jcd/batman/vdisk/search/VirtualDiskSearch.java
+++ core/src/ch/se/inf/ethz/jcd/batman/vdisk/search/VirtualDiskSearch.java
@@ -3,6 +3,7 @@ package ch.se.inf.ethz.jcd.batman.vdisk.search;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import ch.se.inf.ethz.jcd.batman.io.VDiskFile;
 
@@ -103,6 +104,31 @@ public class VirtualDiskSearch {
 			}
 
 			if (settings.checkSubFolders()) {
+				foundEntries.addAll(searchName(settings, term, children));
+			}
+		}
+
+		return foundEntries;
+	}
+
+	public static List<VDiskFile> searchName(Settings settings, Pattern term,
+			VDiskFile... parents) throws IOException {
+		List<VDiskFile> foundEntries = new LinkedList<>();
+
+		for (VDiskFile parent : parents) {
+			VDiskFile[] children = parent.listFiles();
+			for(VDiskFile child : children) {
+				boolean check = (settings.checkFiles() && child.isFile())
+						|| (settings.checkFolders && child.isDirectory());
+				
+				if(check) {
+					if(term.matcher(child.getName()).find()) {
+						foundEntries.add(child);
+					}
+				}
+			}
+			
+			if(settings.checkSubFolders()) {
 				foundEntries.addAll(searchName(settings, term, children));
 			}
 		}
