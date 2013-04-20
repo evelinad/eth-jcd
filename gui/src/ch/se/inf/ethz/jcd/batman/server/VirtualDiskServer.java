587f4e01c46e502ec5292b232fb4a85d25cac89b
Gregor Wegberg <github@gregorwegberg.com>
2013-04-20 12:10:15 +0200

    Added JavaDoc to VirtualDiskServer

--- gui/src/ch/se/inf/ethz/jcd/batman/server/VirtualDiskServer.java
+++ gui/src/ch/se/inf/ethz/jcd/batman/server/VirtualDiskServer.java
@@ -5,16 +5,14 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
+/**
+ * Starting point for the virtual disk server.
+ *
+ */
 public class VirtualDiskServer {
 
 	public final static String SERVICE_NAME = "VirtualDisk";
 
-	/*
-	 * set
-	 * CLASSPATH=C:\Users\Benji\Documents\GitHub\eth-jcd\gui\bin;C:\Users\Benji
-	 * \Documents\GitHub\eth-jcd\core\bin "C:\Program
-	 * Files\Java\jdk1.7.0_17\bin\rmiregistry.exe"
-	 */
 	public static void main(String[] args) {
 		try {
 			String name = SERVICE_NAME;
