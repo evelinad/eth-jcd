29b6fd705aa008aef14c5db72a9f8088401be636
Gregor Wegberg <github@gregorwegberg.com>
2013-04-20 12:56:40 +0200

    added JavaDoc

--- gui/src/ch/se/inf/ethz/jcd/batman/controller/TaskControllerFactory.java
+++ gui/src/ch/se/inf/ethz/jcd/batman/controller/TaskControllerFactory.java
@@ -4,16 +4,30 @@ import java.net.URI;
 
 import ch.se.inf.ethz.jcd.batman.controller.remote.RemoteTaskController;
 
+/**
+ * Factory to get the right {@link TaskController} for a given {@link URI}.
+ * 
+ * 
+ */
 public class TaskControllerFactory {
 
 	private static final String REMOTE_SCHEME = "batman";
-	
-	public static TaskController getController (URI uri) {
+
+	/**
+	 * Returns the right {@link TaskController} for the given {@link URI}.
+	 * 
+	 * @param uri
+	 *            the location of the virtual disk
+	 * @return a {@link TaskController} that can execute commands on the given
+	 *         virtual disk
+	 */
+	public static TaskController getController(URI uri) {
 		if (REMOTE_SCHEME.equals(uri.getScheme())) {
 			return new RemoteTaskController(uri);
 		} else {
-			throw new IllegalArgumentException("Unsupported scheme: " + uri.getScheme());
+			throw new IllegalArgumentException("Unsupported scheme: "
+					+ uri.getScheme());
 		}
 	}
-	
+
 }
