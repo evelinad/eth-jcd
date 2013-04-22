c2d14a69ffe65b5c733177a1495d92d378284ecf
Gregor Wegberg <github@gregorwegberg.com>
2013-04-22 20:11:36 +0200

    forgot CreateDirectoryDialog

---
+++ gui/src/ch/se/inf/ethz/jcd/batman/browser/CreateDirectoryDialog.java
@@ -0,0 +1,64 @@
+package ch.se.inf.ethz.jcd.batman.browser;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+public class CreateDirectoryDialog extends ModalDialog {
+
+	private TextField inputField;
+
+	public CreateDirectoryDialog() {
+		super();
+		setTitle("Create Directory");
+		
+		Label label = new Label("Directory Name:");
+		getContainer().add(label, 0, 0);
+		
+		inputField = new TextField();
+		inputField.setPromptText("URI to a virtual disk");
+		inputField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.ENTER) {
+					setCloseReason(CloseReason.OK);
+					close();
+				}
+			}
+		});
+		getContainer().add(inputField, 1, 0);
+
+		Button okButton = new Button("Connect");
+		okButton.setDefaultButton(true);
+		okButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				setCloseReason(CloseReason.OK);
+				close();
+			}
+		});
+		getContainer().add(okButton, 0, 1);
+
+		Button cancelButton = new Button("Cancel");
+		cancelButton.setCancelButton(true);
+		cancelButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				inputField.setText("");
+				setCloseReason(CloseReason.CANCEL);
+				close();
+			}
+		});
+		getContainer().add(cancelButton, 1, 1);
+		
+		inputField.requestFocus();
+	}
+
+	public String getUserInput() {
+		return inputField.getText();
+	}
+}
